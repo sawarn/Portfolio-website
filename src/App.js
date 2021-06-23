8c692e19af4490cfea0e74558024a6961c399448
Shivam <shivamsawarn15@gmail.com>
2021-06-23 14:08:11 +0000

    Update by shivam

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
+import Navbar from './components/Navbar'
 import './App.css';
-
+import "/home/ga1ileo/Desktop/Portfolio/node_modules/bootstrap/dist/css/bootstrap.min.css"
+import Skills from './components/Skills';
+import Header from './components/Header'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Navbar/>
+    <Header/>
+    
+    </>
   );
 }
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
+import {faBars} from "@fortawesome/free-solid-svg-icons"
+const Navbar = () => {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-dark">
+<div className= "container">
+  <a className="navbar-brand" href="#">Shivam Sawarn</a>
+  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <FontAwesomeIcon icon={faBars} style={{color:'#FFA62F'}}/>
+  </button>
+
+  <div className="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul className="navbar-nav ml-auto">
+      <li className="nav-item active">
+        <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="#">About Me</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="#">Skills</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="#">Projects</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="#">Acheivements</a>
+      </li>
+     
+    </ul>
+   
+  </div>
+</div>            
+</nav>
+    )
+}
+
+export default Navbar
+
+
