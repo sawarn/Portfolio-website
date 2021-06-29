76bfcccb2c63011db819eb0121ca9d79725d1bd7
Shivam <shivamsawarn15@gmail.com>
2021-06-29 01:14:52 +0000

    Update by shivam

--- src/components/Aboutme.js
+++ src/components/Aboutme.js
@@ -1,10 +1,15 @@
 import React from "react"
-
+import img from "./assets/myself.jpg"
 const About = () =>{
     return(
         <div className="about">
-            <h1>About</h1>
-            <div></div>
+            <h1>About Me</h1><br>
+            </br>
+            <img src={img}></img>
+            <div className="abt-cont">
+            <p>Delhi is one of the oldest cities in the world, and has been continuously inhabited since the 6th century BCE.[22] Through most of its history, Delhi has served as a capital of various kingdoms and empires, most notably the Tomars, Chahamanas, Delhi Sultanate and Mughals. It is also assumed to be the location of Indraprastha, the capital of the Pandavas in the epic Mahabharata. The city has been captured, ransacked and rebuilt several times, particularly during the medieval period, and modern Delhi is a cluster of a number of cities spread across the metropolitan region. 
+            </p>
+            </div>
         </div>
     )
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,6 @@ import {faBars} from "@fortawesome/free-solid-svg-icons"
 const Navbar = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-dark">
-<div className= "container">
   <a className="navbar-brand" href="#">Shivam Sawarn</a>
   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <FontAwesomeIcon icon={faBars} style={{color:'#FFA62F'}}/>
@@ -30,7 +29,6 @@ const Navbar = () => {
      
     </ul>
    
-  </div>
 </div>            
 </nav>
     )
